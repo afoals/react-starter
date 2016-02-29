559fdbe805b04c2b3358bba7600fb6c1fa40fc32
Peter Tait <contact@petertait.com>
2016-02-29 09:54:59 +0000

    fix undefined comp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "babel-polyfill";
 
-import React from "react";
+import React, {Component, PropTypes} from 'react';
 import ReactDOM from "react-dom";
 import { Router, browserHistory } from "react-router";
 
